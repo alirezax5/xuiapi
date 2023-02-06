22da4241bd57f18b68ec457168888e0080272aaf
alireza x5 <alirezaaghax6@gmail.com>
2023-02-07 02:03:29 +0330

    Create listInbound.php

---
+++ Examples/listInbound.php
@@ -0,0 +1,8 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+$xui = new \alirezax5\XuiApi\Panel\Vaxilu('YOU_PANEL_URL', 'YOU_PANEL_USERNAME', 'YOU_PANEL_PASSWORD');
+$xui->setCookie(__DIR__.'/a.txt');
+$xui->login();
+
+
+$list = $xui->listInbound();
