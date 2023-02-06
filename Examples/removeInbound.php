bb03b76650533e6f2e1343eefef6987f42f05cf1
alireza x5 <alirezaaghax6@gmail.com>
2023-02-07 02:06:35 +0330

    Create removeInbound.php

---
+++ Examples/removeInbound.php
@@ -0,0 +1,7 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+$xui = new \alirezax5\XuiApi\Panel\Vaxilu('YOU_PANEL_URL', 'YOU_PANEL_USERNAME', 'YOU_PANEL_PASSWORD');
+$xui->setCookie(__DIR__ . '/a.txt');
+$xui->login();
+
+$xui->removeInbound(1);
