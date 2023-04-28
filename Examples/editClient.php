83d1da395532c47af1cf6a88a3b2b26af0e2f6c9
alireza x5 <alirezaaghax6@gmail.com>
2023-04-28 13:55:23 +0330

    Update editClient.php

--- Examples/editClient.php
+++ Examples/editClient.php
@@ -1,12 +1,12 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
-$xui = new \alirezax5\XuiApi\Panel\Vaxilu('YOU_PANEL_URL', 'YOU_PANEL_USERNAME', 'YOU_PANEL_PASSWORD');
+$xui = new \alirezax5\XuiApi\Panel\MHSanaei('YOU_PANEL_URL', 'YOU_PANEL_USERNAME', 'YOU_PANEL_PASSWORD');
 $xui->setCookie(__DIR__.'/a.txt');
 $xui->login();
 
-$xui->editClient('inbound id[int]','uuid or password[string]','email[string]','uuid or password[string]','traffic[int Gb]','time_ms[big int]','tgId[string]','subId[string]','limitIp[int]','fingerprint[string]','flow[string]');
+$xui->editClient('inbound id[int]','uuid or password[string]','email[string]','uuid or password[string]','traffic[int Gb]','time_ms[big int]','tgId !just MHSanaei panel[string]','subId !just MHSanaei panel [string]','limitIp[int]','fingerprint[string]','flow[string]');
 
-$xui->editClientByEmail('inbound id[int]','email[string]','email[string]','uuid or password[string]','traffic[int Gb]','time_ms[big int]','tgId[string]','subId[string]','limitIp[int]','fingerprint[string]','flow[string]');
+$xui->editClientByEmail('inbound id[int]','email[string]','email[string]','uuid or password[string]','traffic[int Gb]','time_ms[big int]','tgId !just MHSanaei panel [string]','subId !just MHSanaei panel [string]','limitIp[int]','fingerprint[string]','flow[string]');
 
 $xui->editClientWithKey('inbound id[int]','uuid or password[string]','key[string]','value');
 
