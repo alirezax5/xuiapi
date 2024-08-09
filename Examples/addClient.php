ce9190b562337dfcc8049bbc3dd5bd065eba10e6
alirezax5 <alirezaaghax6@gmail.com>
2024-08-09 15:59:24 +0330

    update

--- Examples/addClient.php
+++ Examples/addClient.php
@@ -5,3 +5,7 @@ $xui->setCookie(__DIR__.'/a.txt');
 $xui->login();
 
 var_dump($xui->addnewClient('inbound id[int]','uuid or password[string]','email[string]','subId[string]','tgid[string]','flow[string]','traffic[int Gb]','time_ms[big int]','limitIp[int]','fingerprint[string]','isTrojan[bool]'));
+
+
+// php 8
+$xui->addnewClient(id:123,uuid: 'uuid',email: 'email',totalgb: 0,eT: 0);
